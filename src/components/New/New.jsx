125521253fa4d8ede1db2759492d2da181873ac0
Neha Dhaka <nehadhaka43@gmail.com>
2023-05-31 12:49:16 -0400

    add form submit

--- src/components/New/New.jsx
+++ src/components/New/New.jsx
@@ -1,10 +1,14 @@
 import "./New.scss";
 import axios from "axios";
 import { useState, useEffect } from "react";
+import { v4 as uuid } from "uuid";
 
 export default function New() {
+  const unique_id = uuid();
+  const small_id = unique_id.slice(0, 8);
   const [employeesList, setEmployeesList] = useState([]);
-  const [assignedEmployeeId, setAssignedEmployeeId] = useState(null);
+  const [assignedEmployeeId, setAssignedEmployeeId] = useState(0);
+  const [description, setDescription] = useState("");
   let isSelected = false;
   useEffect(() => {
     axios.get("http://localhost:8080/employees").then((response) => {
@@ -16,13 +20,31 @@ export default function New() {
     return <h1>Loading...</h1>;
   }
 
-  function handleOnClick() {
+  function handleOnClick(employeeId) {
+    setAssignedEmployeeId(employeeId);
     isSelected = true;
+  }
+
+  function handleChange(event) {
+    const data = event.target.value;
+    setDescription(data);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    const formData = {
+      id: small_id,
+      description: description,
+      isCompleted: false,
+      assignedEmployeeId: assignedEmployeeId,
+      dueDate: 10,
+    };
+    axios.post("http://localhost:8080/tasks", formData);
   }
   return (
     <section className="new">
       <h1 className="new__heading">New Task</h1>
-      <form className="new__form">
+      <form onSubmit={handleSubmit} className="new__form">
         <textarea
           name="description"
           id="description"
@@ -30,14 +52,22 @@ export default function New() {
           cols="30"
           rows="10"
           placeholder="Enter a description.."
+          value={description}
+          onChange={handleChange}
         ></textarea>
-        <input type="hidden" name="hiddenField" value="hiddenValue"></input>
+        <input
+          type="hidden"
+          name="hiddenField"
+          value={assignedEmployeeId}
+        ></input>
         <ul className="employees__container">
           {employeesList.map((employee) => (
             <li
-              onClick={handleOnClick}
+              onClick={() => {
+                handleOnClick(employee.id);
+              }}
               key={employee.id}
-              className={`new__item ${isSelected && "new__selected"}`}
+              className={`new__item`}
             >
               <img className="new__img" src={employee.image} alt="" />
               <p className="new__name">{employee.name}</p>
